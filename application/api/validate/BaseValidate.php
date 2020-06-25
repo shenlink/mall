04463f2e1f5dd9aa13f9d2793b38780e6610a41d
shenlink <2024133718@qq.com>
2020-06-25 10:48:22 +0800

    feat:验证器基类

---
+++ application/api/validate/BaseValidate.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\api\validate;
+
+use think\Validate;
+
+/*
+ * @Description:验证器基类
+ * @Author: shenlink
+ * @Date: 2020-06-24 22:18:56
+ * @LastEditors: shenlink
+ * @LastEditTime: 2020-06-25 10:48:01
+ */
+
+class BaseValidate extends Validate
+{
+
+}
