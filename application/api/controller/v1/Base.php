4d171386a32b72cce69f5dc7994aa9e8439b673c
shenlink <2024133718@qq.com>
2020-06-26 11:48:53 +0800

    feat:控制器基类

---
+++ application/api/controller/v1/Base.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\api\controller\v1;
+
+use think\Controller;
+
+/*
+ * @Description: shenlink
+ * @Author: shenlink
+ * @Date: 2020-06-24 21:22:35
+ * @LastEditors: shenlink
+ * @LastEditTime: 2020-06-24 21:59:20
+ */
+
+class Base extends Controller
+{
+
+}
